0dd8b6a72371eabddbd2b3bb2bbb06335cab8e54
John Gruss <JohnGruss@Deltek.com>
2020-12-10 09:05:23 -0500

    Solution for Day3_Part1

--- PowerShell/Day3_Part1.ps1
+++ PowerShell/Day3_Part1.ps1
@@ -1,1 +1,25 @@
+$input = get-content "$psscriptroot\Day3_input.txt"
 
+$Map = new-object system.collections.generic.list[string[]]
+$map.add($input)
+
+do{
+    $map = $map | foreach-object{
+        $temp = $_ *= 2
+        $temp
+    }
+}
+Until ($map[-1].length -gt ($input.count * 3))
+
+$Position = @(0,0)
+$TreeCount = 0
+do{
+    $position[0] ++
+    $position[1] = $position[1] + 3
+    if(($map[$position[0]])[$position[1]] -match '#') {
+        $treecount ++
+    }
+}
+Until ($position[0] -ge $input.count)
+
+write-output $Treecount
