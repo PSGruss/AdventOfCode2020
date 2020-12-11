f51d668b8ab04e9e3f34c73505f62d4f10683786
John Gruss <JohnGruss@Deltek.com>
2020-12-11 15:46:30 -0500

    Day6_part2 Solution!

--- PowerShell/Day6/Day6_part2.ps1
+++ PowerShell/Day6/Day6_part2.ps1
@@ -5,9 +5,15 @@ $Input_Join = $input -join $s
 
 $output = [system.collections.generic.list[int]]::new()
 foreach($row in ($input_join -split "$($s)$($s)")) {
+    #Seperate string into the group answer
+    $GroupAnswer = $Row -split $s
+    #consolidate group answer into row of letters
     $row_replace = $row -replace $s,''
-    $unique_Letters  = $row_replace.ToCharArray() | group-object | select-object -expand name
-    $Unique_Letters_Row = $unique_Letters -join ''
-    $output.add($Unique_Letters_Row.length)
+    #get the answers that is shared with each group member
+    $unique_Letters = $row_replace.ToCharArray() | group-object | ?{$_.count -match $GroupAnswer.count} 
+    #Count the number of matching letters.
+    #Note, $unique_letters has a property called Count, so measure-object is used to count the letters.
+    $a = $unique_Letters | measure-object | select -expand Count
+    $output.add($a)
 }
 $output | measure-object -sum | select -expand sum
