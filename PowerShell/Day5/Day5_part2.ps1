45b414c11f518ad1105424a78abed86e7147c7cd
John Gruss <JohnGruss@Deltek.com>
2020-12-11 13:12:19 -0500

    Day5_Part2 Solution!

--- PowerShell/Day5/Day5_part2.ps1
+++ PowerShell/Day5/Day5_part2.ps1
@@ -2,39 +2,40 @@ $input = get-content "$psscriptroot\Day5_input.txt"
 
 #Boarding a plane
 
+$AllSeatIDs = @()
 $AllSeats = @()
-
 foreach($row in $input) {
-    $row
-    $Seat = @{X = 0,Y = 0}
-    $x = 8
+    $Seat = [pscustomobject]@{SeatID="";X = 0;Y = 0}
+    $x = 4
     $y = 64
-    foreach($letter in $row) {
-        if ($letter -match 'F'){
+    $row -split '' | foreach-object {
+        if ($_ -match 'F'){
             # do not add value to $Seat
             # divide $y value
             $y /= 2
         }
-        Elseif ($letter -match 'B'){
+        Elseif ($_ -match 'B'){
             # do add value to $Seat
-            $seat['Y'] += $y
+            $seat.y += $y
             # divide $y value
             $y /= 2
         }
-        Elseif ($letter -match 'L') {
+        Elseif ($_ -match 'L') {
             # do not add value to $Seat
             # divide $x value
             $x /= 2
         }
-        Elseif ($letter -match 'R') {
+        Elseif ($_ -match 'R') {
             # do add value to $Seat
-            $seat['X'] += $x
+            $seat.x += $x
             # divide $x value in half
             $x /= 2
         }
     }
-    $SeatID = $Seat['X'] * 8 + $Seat['Y']
-    $AllSeats += $SeatID
+    $SeatID = ($Seat.y) * 8 + ($Seat.x)
+    $seat.SeatID = $SeatID
+    $AllSeats += $Seat    
+    $AllSeatIDs += $SeatID
 }
-$AllSeats
+$AllSeats | group y | ?{$_.count -eq 7} | select -expand group
 
